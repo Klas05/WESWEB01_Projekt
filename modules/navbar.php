c2e7703904d8e6822b7d6683b53a4feaeb57f78d
TSSReal <thesweslayer@gmail.com>
2024-04-16 21:24:52 +0200

    Beggining

---
+++ modules/navbar.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Skivor AB</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+
+  <nav>
+    <a href="./index.php">
+      <h1>Skivor AB</h1>
+    </a>
+    <a href="./albums.php">
+      <h1>Skivor</h1>
+    </a>
+    <a href="./artists.php">
+      <h1>Artister</h1>
+    </a>
+  </nav>
+
+  <?php
+
+  require_once "connector.php";
+
+  try {
+    $pdo = connectToDb();
+  } catch (PDOException $e) {
+    die("Fel: " . $e->getMessage());
+  }
