454fe09661d4d41eeab7004d2fee71b94dfe1983
TSSReal <thesweslayer@gmail.com>
2024-04-18 11:23:06 +0200

    progress

--- functions/functions.php
+++ functions/functions.php
@@ -9,7 +9,44 @@ function getData($sql): array
   } catch (PDOException $e) {
     die("Fel: " . $e->getMessage());
   }
-  $stmt = $pdo->query($sql);
-  $res = $stmt->fetchAll();
+  $res = $pdo->query($sql)->fetchAll();
+  // $res = $stmt->fetchAll();
   return $res;
+}
+
+function getMonthName($monthNumber): string
+{
+  // Skapa en array med månadernas namn
+  $monthNames = array(
+    "Januari",
+    "Februari",
+    "Mars",
+    "April",
+    "Maj",
+    "Juni",
+    "Juli",
+    "Augusti",
+    "September",
+    "Oktober",
+    "November",
+    "December"
+  );
+
+  // Kontrollera om angivet månadsvärde är giltigt
+  if ($monthNumber >= 1 && $monthNumber <= 12) {
+    // Returnera månadens namn från arrayen
+    return $monthNames[$monthNumber];
+  } else {
+    // Om ogiltigt värde, returnera en felmeddelande-sträng
+    return "Ogiltigt månadsvärde";
+  }
+}
+
+function getLatestAlbumSongs(): void
+{
+  $sql = "SELECT * FROM start_songs";
+  $songs = getData($sql);
+  foreach ($songs as $song) {
+    echo "<tr><td>" . $song["name"] . "</td>" . "<td>" . $song["duration"] . "</td>";
+  }
 }
--- index.php
+++ index.php
@@ -4,15 +4,27 @@ include("modules/navbar.php");
 
 require_once "functions/functions.php";
 
-$sql = "SELECT * FROM albums ORDER BY import_date LIMIT 1";
+$sql = "SELECT * FROM start";
 
-$res = getData($sql);
+$album = getData($sql);
 
 ?>
 
 <div class="wrapper">
-  <img src="<?php echo $res[0]["img"]; ?>" alt="">
-
+  <img src="<?php echo $album[0]["img"]; ?>" alt="">
+  <div class="info">
+    <h2><?php echo $album[0]["artist"] . " - ";
+        echo $album[0]["name"]; ?></h2>
+    <h3><?php echo "Släpptes " . $album[0]["YEAR"] . " den " . $album[0]["DAY"] . " " . getMonthName($album[0]["MONTH"]); ?></h3>
+    <h4>Låtar:</h4>
+    <table>
+      <tr>
+        <th>Låt namn</th>
+        <th>Längd</th>
+      </tr>
+      <?php getLatestAlbumSongs(); ?>
+    </table>
+  </div>
 </div>
 
 <?php
